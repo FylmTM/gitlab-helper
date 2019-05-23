66da3d10ec389e26c1dcdec36649f61a3b74c8dc
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2019-05-23 13:46:35 +0300

    Check is checkbox selected

--- gitlab-helper.js
+++ gitlab-helper.js
@@ -2,10 +2,10 @@ function selectCheckbox() {
     const mergeRequestForceRemoveSourceBranch = document.getElementById("merge_request_force_remove_source_branch");
     const mergeRequestSquash = document.getElementById("merge_request_squash");
 
-    if (mergeRequestForceRemoveSourceBranch != null) {
+    if (mergeRequestForceRemoveSourceBranch != null && mergeRequestForceRemoveSourceBranch.checked === false) {
         mergeRequestForceRemoveSourceBranch.click();
     }
-    if (mergeRequestSquash != null) {
+    if (mergeRequestSquash != null && mergeRequestSquash.checked === false) {
         mergeRequestSquash.click();
     }
 }
