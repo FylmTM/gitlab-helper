e25a8debea106ce5dc051f98cb8c17a1d289f1cf
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2019-06-04 18:38:32 +0300

    Sort by name

--- gitlab-helper.js
+++ gitlab-helper.js
@@ -1,3 +1,4 @@
+// Select merge request checkboxes
 function selectCheckbox() {
     const mergeRequestForceRemoveSourceBranch = document.getElementById("merge_request_force_remove_source_branch");
     const mergeRequestSquash = document.getElementById("merge_request_squash");
@@ -15,3 +16,16 @@ document.addEventListener('DOMContentLoaded', (e) => {
 });
 
 selectCheckbox();
+
+// Sort by name by default
+const links = document.querySelectorAll('.js-group-filter-dropdown-wrap ul li a');
+const sortByNameAscLink = [...links.values()].find((link) => link.href.endsWith("name_asc"));
+
+if (sortByNameAscLink) {
+    if (!sortByNameAscLink.className.includes("is-active")) {
+        const params = new URLSearchParams(window.location.search);
+        if (!params.has('sort')) {
+            sortByNameAscLink.click();
+        }
+    }
+}
